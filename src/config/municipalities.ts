24d61e810ad0841af22c344bad4502a4e2a8007e
appyem-de-github <gerenteappyempresa@gmail.com-en-github.com>
2026-02-01 17:17:46 -0500

    fix: puestos de votacion

--- src/config/municipalities.ts
+++ src/config/municipalities.ts
@@ -1,4 +1,4 @@
-// Datos de municipios de Caldas con sus puestos de votación
+// Datos de municipios de Caldas con sus puestos de votación oficiales
 export interface Municipality {
   id: string;
   name: string;
@@ -12,10 +12,97 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Manizales', 
     tables: 902,
     votingPlaces: [
-      "Las Américas", "Coliseo Mayor", "Gobernación de Caldas", 
-      "Universidad de Caldas", "Liceo Departamental", "Escuela Normal", 
-      "Centro de Convenciones", "Institución Educativa Boston", 
-      "Coliseo Menor", "Escuela Antonio Nariño"
+      "I.E LA LINDA",
+      "I.E CHIPRE",
+      "I.E CHIPRE SEDE B",
+      "I.E ADOLFO HOYOS ARANGO",
+      "COLEGIO FILIPENSE",
+      "I.E MARCO FIDEL SUAREZ ANT. CASD",
+      "NUEVO CISCO SAN JOSE",
+      "CEN. DE DESARROLLO INFANTIL JOSE MARIA",
+      "INSTITUTO MANIZALES",
+      "ANTIGUO CISCO SAN JOSE",
+      "ANTIGUO TERMINAL DE TRANSPORTE PEOPLE CONTACT",
+      "EDIFICIO INDUSTRIA LICORERA DE CALDAS",
+      "FACULTAD DE BELLAS ARTES UNIVERSIDAD DE CALDAS",
+      "I.E GRAN COLOMBIA",
+      "LICEO ARQUIDIOCESANO NUESTRA SEÑORA",
+      "I.E ISABEL LA CATOLICA",
+      "INSTITUTO UNIVERSITARIO DE CALDAS",
+      "INSTITUTO TECNOLOGICO FRANCISCO JOSE DE CALDAS",
+      "LICEO ARQUIDIOCESANO NUESTRA SEÑORA FEMENINO",
+      "I.E LA ASUNCION SEDE A",
+      "INSTITUTO SAN JORGE",
+      "FACULTAD CIENCIAS SALUD UNI CALDAS",
+      "I.E NORMAL SUPERIOR DE CALDAS",
+      "UNIVERSIDAD AUTONOMA",
+      "I.E FE Y ALEGRIA LA PZ SEDE PRINCIPAL",
+      "I.E BOSQUES DEL NORTE",
+      "I.E SAN SEBASTIAN",
+      "CISCO BOSQUES DEL NORTE",
+      "I.E FE Y ALEGRIA SEDE B",
+      "ESC. NACIONAL DE ENFERMERIA SEDE A",
+      "I.E LA SULTANA SEDE PRINCIPAL",
+      "LICEO INTEGRADO MANIZALES",
+      "I.E MARISCAL SUCRE SEDE B",
+      "INSTITUTO COLEGIO DE CRISTO",
+      "I.E LA SULTANA SEDE A",
+      "I.E MARISCAL SUCRE SEDE C KENNEDY",
+      "COLEGIO SANTA INES",
+      "I.E SAN PIO X",
+      "I.E SAN PIO X SEDE B",
+      "I.E SAN PIO X SEDE A",
+      "I.E MALTERIA",
+      "I.E SAN PIO X SEDE C LA CAPILLA",
+      "UNIVERSIDAD CATOLICA MANIZALES",
+      "FACULTAD CIENCIAS JURIDICAS U. DE CALDAS",
+      "INST NACIONAL AUX DE ENFERMERIA SEDE PRINCIPAL",
+      "I.E INSTITUTO NORMAL SUPERIOR DE MANIZALES",
+      "SEM MENOR NUESTRA SEÑORA DEL ROSARIO",
+      "UNI NACIONAL SEDE ARQUITECTURA",
+      "CENTRO COMERCIAL SANCANCIO",
+      "COLISEO MENOR RAMON MARIN",
+      "I.E ATANACIO GIRARDOT",
+      "I.E EUGENIO PACELLI SEDE PRINCIPAL",
+      "I.E MALABAR SEDE A",
+      "I.E ARANJUEZ SEDE A",
+      "I.E MALABAR SEDE B",
+      "U DE CALDAS SEDE BICENTENARIO MICAELA",
+      "I.E ANDRES BELLO",
+      "I.E PERPETUO SOCORRO",
+      "I.E LEONARDO DAVINCI SEDE A",
+      "I.E LEONARDO DAVINCI",
+      "I.E LATINOAMERICANO SEDE B",
+      "COL MAYOR NUESTRA SEÑORA",
+      "I.E LEON DE GREIFF",
+      "I.E SIETE DE AGOSTO",
+      "I.E EDUCATIVA ESTAMBUL",
+      "I.E PABLO VI BARRIOS UNIDOS",
+      "CISCO EL CARMEN",
+      "I.E SINAI SEDE B",
+      "I.E LA ASUNCION SEDE B",
+      "I.E LICEO MIXTO SINAI SEDE",
+      "C DE DESARROLLO INFANTIL VILLAHERMOSA",
+      "CENTRO COMERCIAL MALL PLAZA",
+      "UNIVERSIDAD DE CALDAS",
+      "CARCEL NACIONAL DE VARONES",
+      "RECLUSION MUJERES VILLA JOSEFINA",
+      "ESCUELA DE TRABAJO LA LINDA LOS ZAGALES",
+      "COLOMBIA",
+      "LA CRISTALINA",
+      "EL REMANZO",
+      "I.E SAN PEREGRINO",
+      "AGROTURISTICO EL TABLAZO",
+      "EL MANANTIAL",
+      "RIO BLANCO",
+      "I.E MIGUEL ANTONIO CARO BACHILLERATO",
+      "EL DESQUITE",
+      "LA VIOLETA",
+      "ALTO CORINTO",
+      "LA TRINIDAD",
+      "ESC RURAL LA AURORA",
+      "ESC RURAL EL ARENILLO",
+      "I.E RURAL GRANADA SEDE PRINCIPAL"
     ]
   },
   { 
@@ -23,9 +110,23 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'La Dorada', 
     tables: 160,
     votingPlaces: [
-      "Coliseo Municipal", "Institución Educativa Técnica", 
-      "Centro de Convenciones", "Escuela Rural La Dorada", 
-      "Coliseo Cubierto", "Casa de la Cultura"
+      "SEDE ANTONIO JOSE SUCRE",
+      "ESCUELA SAN VICENTE",
+      "I.E LA DORADA",
+      "I.E NUESTRA SEÑORA DEL CARMEN",
+      "ESCUELA ANTONIO NARIÑO",
+      "ESCUELA LAURELES",
+      "ESCUELA POLICARPA SALAVARRIETA",
+      "I.E ALFONSO LOPEZ",
+      "ESC JUAN PABLO SEGUNDO",
+      "I.E MARCO FIDEL SUAREZ",
+      "ESC RENAN BARCO",
+      "PENITENCIARIA DOÑA JUANA",
+      "BUENA VISTA LA HABANA",
+      "EL PURNIO",
+      "EL JAPON",
+      "GUARINOSITO",
+      "LA ATARRAYA"
     ]
   },
   { 
@@ -33,8 +134,18 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Chinchiná', 
     tables: 107,
     votingPlaces: [
-      "Escuela Normal", "Coliseo Cubierto", "Casa de la Cultura", 
-      "Institución Educativa Técnica", "Centro Comunitario"
+      "I.E SAN FRANCISCO DE PAULA",
+      "COL BRTOLOME MITRE",
+      "BIBLIOTECA PUBLICA MUNICIPAL",
+      "SANTA JUANA DE ARCO",
+      "JARDIN INFANTIL CHINCHINA",
+      "I.E SANTO DOMINGO SAVIO",
+      "ESCUELA KENNEDY",
+      "COLISEO MUNICIPAL",
+      "ESCUELA JUAN XXIII",
+      "BAJO ESPAÑOL",
+      "EL TREBOL",
+      "LA FLORESTA"
     ]
   },
   { 
@@ -42,9 +153,26 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Riosucio', 
     tables: 104,
     votingPlaces: [
-      "Institución Educativa San José", "Coliseo Municipal", 
-      "Casa de la Juventud", "Escuela Rural Riosucio", 
-      "Centro Cultural"
+      "CONC ESCOL SANTANDER",
+      "CONC ESCOL ANTONIO NARIÑO",
+      "I.E NORMAL SUPERIOR SAGRADO CORAZON",
+      "I.E FUNDADORES",
+      "COLISEO DE DEPORTES",
+      "CARCEL DEL CIRCUITO",
+      "BONAFONT",
+      "PUEBLO VIEJO",
+      "EL ORO",
+      "EL SALADO",
+      "FLORENCIA",
+      "LOS CHANCOS",
+      "QUIEBRALOMO",
+      "SAN LORENZO",
+      "SAN JERONIMO",
+      "LA IBERIA",
+      "LLANOGRANDE",
+      "LAS ESTANCIAS",
+      "MEJIAL",
+      "SIPIRRA"
     ]
   },
   { 
@@ -52,8 +180,26 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Villamaría', 
     tables: 100,
     votingPlaces: [
-      "Coliseo Cubierto", "Escuela Rural", "Centro Comunitario", 
-      "Institución Educativa Villamaría", "Casa de la Cultura"
+      "I.E JAIME DUQUE GRISALES",
+      "I.E SANTA LUISA",
+      "COL VILLA DEL ROSARIO",
+      "TURIN CRC",
+      "COL SAN PEDRO CLAVER",
+      "I.E GERARDO ARIAS RAMIREZ",
+      "COLISEO JAIME DUQUE GRISALES",
+      "ESC RAFAEL POMBO",
+      "CDI CHIQUITINES",
+      "CDI FLORECER",
+      "I.E NUESTRA SEÑORA DEL ROSARIO",
+      "EL PINDO ESCUELA ANTONIO NARIÑO",
+      "I.E COLOMBIA LA GUAYANA",
+      "LLANITOS",
+      "NUEVO RIO CLARO",
+      "MIRAFLORES",
+      "VRDA LOS CUERVOS CASTA COMUNAL ALTO DE LA CRUZ",
+      "NUEVA PRIMAVERA",
+      "GALLINAZO",
+      "LAGUNA NEGRA"
     ]
   },
   { 
@@ -61,7 +207,15 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Aguadas', 
     tables: 60,
     votingPlaces: [
-      "Coliseo Municipal", "Escuela Normal", "Casa de la Cultura"
+      "Coliseo Cubierto Cambumbia",
+      "LICEO CLAUDINA MUNERA",
+      "ARMA",
+      "ENCIMADAS",
+      "VIVORAL",
+      "LA MERMITA",
+      "ALTO LA MONTAÑA",
+      "BOCAS",
+      "RIO ARRIBA EL EDEN"
     ]
   },
   { 
@@ -69,8 +223,27 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Anserma', 
     tables: 87,
     votingPlaces: [
-      "Institución Educativa Anserma", "Coliseo Municipal", 
-      "Centro Comunitario"
+      "BIBLIOTECA MUNICIPAL",
+      "ANTIGUA SEDE UNIVALLE",
+      "CENTRO VIDA",
+      "ESCUELA URBANA ANTONIO JOSE SUCRE",
+      "ESCUELA SAN JOSE",
+      "ESCUELA SIMON BOLIVAR",
+      "CIC",
+      "ESCUELA NORMAL SUPERIOR DE ANSERMA",
+      "CARCEL",
+      "VEREDA CHAPATA",
+      "MARAPRA",
+      "LA RICA",
+      "EL GUAMITO",
+      "LAS MARGARITAS",
+      "LA INDIA",
+      "PALO BLANCO",
+      "LA FLORESTA",
+      "LA NUBIA",
+      "CONCHARI",
+      "BELLA VISTA",
+      "SAN PEDRO"
     ]
   },
   { 
@@ -78,7 +251,13 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Aranzazu', 
     tables: 37,
     votingPlaces: [
-      "Escuela Rural Aranzazu", "Coliseo Municipal"
+      "COLEGIO PIO XI",
+      "NORMAL SUPERIOR SAGRADO CORAZON",
+      "ALEGRIAS",
+      "LAS CAMELIAS",
+      "BUENOS AIRES LA HONDITA",
+      "VARSOVIA",
+      "EL ROBLAL"
     ]
   },
   { 
@@ -86,7 +265,13 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Belalcázar', 
     tables: 32,
     votingPlaces: [
-      "Institución Educativa Belalcázar", "Coliseo Municipal"
+      "PUESTO CABECERA MUNICIPAL",
+      "EL MADROÑO",
+      "EL AGUILA",
+      "LA CASCADA",
+      "QUIEBRA DE LA HABANA",
+      "SAN ISIDRO",
+      "EL CRUCERO"
     ]
   },
   { 
@@ -94,7 +279,14 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Filadelfia', 
     tables: 30,
     votingPlaces: [
-      "Escuela Rural Filadelfia", "Coliseo Municipal"
+      "CABECERA MUNICIPAL",
+      "EL PINTADO",
+      "EL VERSO",
+      "LA SOLEDAD",
+      "LA PALMA",
+      "MORRITOS",
+      "SAMARIA",
+      "SAN LUIS"
     ]
   },
   { 
@@ -102,7 +294,13 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'La Merced', 
     tables: 19,
     votingPlaces: [
-      "Institución Educativa La Merced", "Coliseo Municipal"
+      "PUESTO CABECERA MUNICIPAL",
+      "EL LIMON",
+      "EL TAMBOR",
+      "LA FELICIA",
+      "MACEGAL",
+      "PEÑA RICA",
+      "SAN JOSE"
     ]
   },
   { 
@@ -110,7 +308,14 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Manzanares', 
     tables: 49,
     votingPlaces: [
-      "Escuela Rural Manzanares", "Coliseo Municipal"
+      "INST EDUC MANZANARES SD SAN LUIS GONZAGA",
+      "AGUA BONITA",
+      "LLANADAS",
+      "LA CEIBA",
+      "LAS MARGARITAS",
+      "LOS PLANES",
+      "SAN JUAN LA SIRIA",
+      "SAN VICENTE"
     ]
   },
   { 
@@ -118,7 +323,13 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Marmato', 
     tables: 21,
     votingPlaces: [
-      "Institución Educativa Marmato", "Coliseo Municipal"
+      "FONDA ASOCAMAR",
+      "CENTRO DE DESARROLLO RURAL CABRAS",
+      "ECHANDIA",
+      "CENTRO POBLADO EL LLANO",
+      "CENTRO DE DESARROLLO RURAL LA MIEL",
+      "SAN JUAN",
+      "LA CUCHILLA"
     ]
   },
   { 
@@ -126,7 +337,10 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Marquetalia', 
     tables: 35,
     votingPlaces: [
-      "Escuela Rural Marquetalia", "Coliseo Municipal"
+      "PUESTO CABECERA MUNICIPAL",
+      "SANTA ELENA",
+      "EL PLACER",
+      "VEREDA PATIO BONITO"
     ]
   },
   { 
@@ -134,7 +348,10 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Marulanda', 
     tables: 10,
     votingPlaces: [
-      "Institución Educativa Marulanda"
+      "PUESTO CABECERA MUNICIPAL",
+      "MONTEBONITO",
+      "EL PARAMO",
+      "EL ZANCUDO"
     ]
   },
   { 
@@ -142,7 +359,15 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Neira', 
     tables: 55,
     votingPlaces: [
-      "Coliseo Municipal", "Escuela Normal", "Casa de la Cultura"
+      "IE NEIRA",
+      "CONCENTRACION ESCOLAR JOSE MARIA CORDOBA",
+      "I.E NUESTRA SEÑORA DEL ROSARIO",
+      "ESCUELA ABRAHAM MONTOYA",
+      "PEBLO RICO",
+      "AGUACATAL",
+      "JUNTAS",
+      "LLANO GRANDE",
+      "EL CARDAL"
     ]
   },
   { 
@@ -150,7 +375,10 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Norcasia', 
     tables: 17,
     votingPlaces: [
-      "Institución Educativa Norcasia", "Coliseo Municipal"
+      "PUESTO CABECERA MUNICIPAL",
+      "CENTRO DE INTEGRACION CIUDADANA CIC",
+      "QUIEBRA DE ROQUE",
+      "MOSCOVITA"
     ]
   },
   { 
@@ -158,7 +386,15 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Pácora', 
     tables: 38,
     votingPlaces: [
-      "Escuela Rural Pácora", "Coliseo Municipal"
+      "PUESTO CABECERA MUNICIPAL",
+      "CARCEL",
+      "CASTILLA",
+      "BUENOS AIRES",
+      "LAS COLES",
+      "LOS MORROS",
+      "SAN BARTOLOME",
+      "LAS PALMAS",
+      "SAN LORENZO"
     ]
   },
   { 
@@ -166,7 +402,10 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Palestina', 
     tables: 46,
     votingPlaces: [
-      "Institución Educativa Palestina", "Coliseo Municipal"
+      "PUESTO CABECERA MUNICIPAL",
+      "ZONA URBANA ARAUCA",
+      "LA PLATA",
+      "SANTAGUEDA"
     ]
   },
   { 
@@ -174,7 +413,19 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Pensilvania', 
     tables: 54,
     votingPlaces: [
-      "Escuela Rural Pensilvania", "Coliseo Municipal", "Casa de la Cultura"
+      "PUESTO CABECERA MUNICIPAL",
+      "CARCEL ARBOLEDA",
+      "AGUA BONITA",
+      "LA SOLEDAD",
+      "SANTA TERESA",
+      "BOLIVIA",
+      "HIGUERON",
+      "GUACAS",
+      "SAN DANIEL",
+      "LA RIOJA",
+      "PUEBLO NUEVO",
+      "SAMARIA",
+      "VEREDA LA TORRE"
     ]
   },
   { 
@@ -182,7 +433,11 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Risaralda', 
     tables: 33,
     votingPlaces: [
-      "Institución Educativa Risaralda", "Coliseo Municipal"
+      "PUESTO CABECERA MUNICIPAL",
+      "ALTO AURACA",
+      "LA BOHEMIA",
+      "LA QUIEBRA DE SANTA BARBARA",
+      "EL PALO"
     ]
   },
   { 
@@ -190,7 +445,13 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Salamina', 
     tables: 47,
     votingPlaces: [
-      "Escuela Rural Salamina", "Coliseo Municipal", "Casa de la Cultura"
+      "PUESTO CABECERA MUNICIPAL",
+      "CARCEL",
+      "LA LOMA",
+      "EL PERRO",
+      "LA UNION",
+      "PORTACHUELO",
+      "I.E SAN FELIX"
     ]
   },
   { 
@@ -198,7 +459,21 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Samaná', 
     tables: 51,
     votingPlaces: [
-      "Institución Educativa Samaná", "Coliseo Municipal"
+      "PUESTO CABECERA MUNICIPAL",
+      "BERLIN",
+      "CALIFORNIA",
+      "EL CONGAL",
+      "YARUMAL",
+      "CONFINES",
+      "SANTA RITA",
+      "FLORENCIA",
+      "ENCIMADAS",
+      "LOS POMOS",
+      "RANCHO LARGO",
+      "SAN DIEGO",
+      "LA QUINTA",
+      "DULCE NOMBRE",
+      "CRISTALES"
     ]
   },
   { 
@@ -206,7 +481,9 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'San José', 
     tables: 15,
     votingPlaces: [
-      "Escuela Rural San José", "Coliseo Municipal"
+      "PUESTO CABECERA MUNICIPAL",
+      "LA ALBANIA",
+      "PRIMAVERA"
     ]
   },
   { 
@@ -214,7 +491,16 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Supía', 
     tables: 61,
     votingPlaces: [
-      "Institución Educativa Supía", "Coliseo Municipal", "Casa de la Cultura"
+      "I.E FRANCISCO JOSE DE CALDAS",
+      "CENTRO CIVICO CULTURAL",
+      "I.E SUPIA",
+      "I.E SAN VICTOR",
+      "I.E MARISCAL ROBLEDO",
+      "I.E POLICARPA SALAVARRIETA",
+      "HOJAS ANCHAS",
+      "GUAMAL",
+      "I.E LA QUINTA",
+      "I.E CAÑAMOMO Y LOMA PRIETA"
     ]
   },
   { 
@@ -222,7 +508,11 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'La Victoria', 
     tables: 27,
     votingPlaces: [
-      "Escuela Rural La Victoria", "Coliseo Municipal"
+      "PUESTO CABECERA MUNICIPAL",
+      "ISAZA",
+      "EL LLANO",
+      "CAÑAVERAL",
+      "LA PRADERA"
     ]
   },
   { 
@@ -230,7 +520,14 @@ export const MUNICIPALITIES: Municipality[] = [
     name: 'Viterbo', 
     tables: 40,
     votingPlaces: [
-      "Institución Educativa Viterbo", "Coliseo Municipal"
+      "PUESTO CABECERA MUNICIPAL",
+      "INSTITUTO EDUCATIVO LA MILAGROSA",
+      "CANAAN",
+      "LA LINDA",
+      "EL PORVENIR",
+      "EL SOCORRO",
+      "LA MARIA",
+      "LA TESALIA"
     ]
   }
 ];
